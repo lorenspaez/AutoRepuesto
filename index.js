fb3a5fab4ab6b546f71f086f530e576d0a4e23af
lorenspaez <54387407+lorenspaez@users.noreply.github.com>
2021-03-07 14:56:34 -0400

    all

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+
+//Settings
+app.set('port', process.env.PORT || 3000 );
+app.use(morgan('dev'));
+
+//static views
+app.use(express.static('views'));
+
+app.listen(app.get('port'), () => {
+	console.log('Server on port', app.get('port'));
+});
